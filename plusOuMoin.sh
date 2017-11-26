6c3980d6821838c5c8396e7a0f1fc62a0bd6113e
x86inside <x86inside@free.fr>
2017-11-26 09:57:13 +0100

    Modification de plusOuMoin.sh ajoute une procédure de saisie en utilisant une expression régulière

--- plusOuMoin.sh
+++ plusOuMoin.sh
@@ -1,6 +1,13 @@
 #! /bin/sh
-
+reponce=0
 nbAleaDizaine=$((`< /dev/urandom tr -dc [:digit:]| head -c1`*10))
 nbAleaUnite=$((`< /dev/urandom tr -dc [:digit:]| head -c1`+1))
 nbAleaFinal=$(($nbAleaDizaine+$nbAleaUnite))
-echo "$nbAleaFinal"
+
+echo "Veuillez saisir un nombre entre 1 et 100"
+read reponce
+if [ -z $(echo "$reponce"|grep -e "^[0-9]\{1,3\}$") ] || [ $reponce -gt 100 ]|| [ $reponce -le 0 ];then
+	echo "Ce n'est pas  un nombre compris entre 1 et 100"
+	echo "Veuillez saisir un nombre entre 1 et 100"
+	reponce=0
+fi
